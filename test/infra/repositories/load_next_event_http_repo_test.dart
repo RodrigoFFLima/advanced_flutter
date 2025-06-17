d0cd538f95823bf7dc78738c4f71127a4479af60
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-16 21:28:09 -0400

    ensure LoadNextEventHttpRepository calls httpClient with correct url

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -9,14 +9,19 @@ import '../../helpers/fakes.dart';
 
 class LoadNextEventHttpRepository {
   final Client httpClient;
-  LoadNextEventHttpRepository({required this.httpClient});
+  final String url;
+
+  LoadNextEventHttpRepository({required this.httpClient, required this.url});
+
   Future<void> loadNextEvent({required String groupId}) async {
-    await httpClient.get(Uri());
+    final uri = Uri.parse(url.replaceAll(':groupId', groupId));
+    await httpClient.get(uri);
   }
 }
 
 class HttpClientSpy implements Client {
   String? method;
+  String? url;
   int callsCount = 0;
 
   @override
@@ -36,6 +41,7 @@ class HttpClientSpy implements Client {
   Future<Response> get(Uri url, {Map<String, String>? headers}) async {
     method = 'get';
     callsCount++;
+    this.url = url.toString();
     return Response('', 200);
   }
 
@@ -91,12 +97,29 @@ class HttpClientSpy implements Client {
 }
 
 void main() {
+  late String groupId;
+  late String url;
+  late HttpClientSpy httpClient;
+  late LoadNextEventHttpRepository sut;
+
+  setUpAll(() {
+    url = 'https://domain.com/api/groups/:groupId/next_event';
+  });
+
+  setUp(() {
+    groupId = anyString();
+    httpClient = HttpClientSpy();
+    sut = LoadNextEventHttpRepository(httpClient: httpClient, url: url);
+  });
+
   test('should request with correct method', () async {
-    final groupId = anyString();
-    final httpClient = HttpClientSpy();
-    final sut = LoadNextEventHttpRepository(httpClient: httpClient);
     await sut.loadNextEvent(groupId: groupId);
     expect(httpClient.method, 'get');
     expect(httpClient.callsCount, 1);
+  });
+
+  test('should request with correct url', () async {
+    await sut.loadNextEvent(groupId: groupId);
+    expect(httpClient.url, 'https://domain.com/api/groups/$groupId/next_event');
   });
 }
