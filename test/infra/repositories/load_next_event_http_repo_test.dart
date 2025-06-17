c8fdd942c58218212805acdf737c53c7baeebdc6
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-16 21:31:22 -0400

    ensure LoadNextEventHttpRepository calls httpClient with correct headers

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -15,7 +15,11 @@ class LoadNextEventHttpRepository {
 
   Future<void> loadNextEvent({required String groupId}) async {
     final uri = Uri.parse(url.replaceAll(':groupId', groupId));
-    await httpClient.get(uri);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await httpClient.get(uri, headers: headers);
   }
 }
 
@@ -23,6 +27,7 @@ class HttpClientSpy implements Client {
   String? method;
   String? url;
   int callsCount = 0;
+  Map<String, String>? headers;
 
   @override
   void close() {}
@@ -42,6 +47,9 @@ class HttpClientSpy implements Client {
     method = 'get';
     callsCount++;
     this.url = url.toString();
+    this.headers =
+        headers ??
+        {'content-type': 'application/json', 'accept': 'application/json'};
     return Response('', 200);
   }
 
@@ -121,5 +129,11 @@ void main() {
   test('should request with correct url', () async {
     await sut.loadNextEvent(groupId: groupId);
     expect(httpClient.url, 'https://domain.com/api/groups/$groupId/next_event');
+  });
+
+  test('should request with correct headers', () async {
+    await sut.loadNextEvent(groupId: groupId);
+    expect(httpClient.headers?['content-type'], 'application/json');
+    expect(httpClient.headers?['accept'], 'application/json');
   });
 }
