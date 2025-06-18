62366941687ac30a48935757aeea22c6e661f31c
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-18 06:13:32 -0400

    ensure LoadNextEventHttpRepository throws unexpectedError on 400

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -11,6 +11,8 @@ import 'package:http/http.dart';
 
 import '../../helpers/fakes.dart';
 
+enum DomainError { unexpected }
+
 class LoadNextEventHttpRepository implements LoadNextEventRepository {
   final Client httpClient;
   final String url;
@@ -25,6 +27,9 @@ class LoadNextEventHttpRepository implements LoadNextEventRepository {
       'accept': 'application/json',
     };
     final response = await httpClient.get(uri, headers: headers);
+    if (response.statusCode == 400) {
+      throw DomainError.unexpected;
+    }
     final event = jsonDecode(response.body);
     return NextEvent(
       groupName: event['groupName'],
@@ -197,5 +202,11 @@ void main() {
     expect(event.players[1].photo, 'photo 2');
     expect(event.players[1].confirmationDate, DateTime(2024, 8, 29, 11, 00));
     expect(event.players[1].isConfirmed, false);
+  });
+
+  test('should throw UnexpectedError on 400', () async {
+    httpClient.statusCode = 400;
+    final future = sut.loadNextEvent(groupId: groupId);
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
