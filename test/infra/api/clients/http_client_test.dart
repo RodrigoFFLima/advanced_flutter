29263fb55f45853f12e4262d015a129cf78121dc
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-22 07:52:31 -0400

    ensure HttpClient throws unexpected error on 400

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_flutter/domain/entities/domain_error.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -21,7 +22,13 @@ class HttpClient {
         'accept': 'application/json',
       });
     final uri = _buildUri(url: url, params: params, queryString: queryString);
-    await client.get(uri, headers: allHeaders);
+    final response = await client.get(uri, headers: allHeaders);
+    switch (response.statusCode) {
+      case 200:
+        break;
+      default:
+        throw DomainError.unexpected;
+    }
   }
 
   Uri _buildUri({
@@ -106,6 +113,36 @@ void main() {
         queryString: {'q1': 'v1', 'q2': 'v2'},
       );
       expect(client.url, 'https://anyurl.com/api/v3/v4?q1=v1&q2=v2');
+    });
+
+    test('should throw UnexpectedError on 400', () async {
+      client.simulateBadRequestError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw SessionExpiredError on 401', () async {
+      client.simulateUnauthorizedError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.sessionExpired));
+    });
+
+    test('should throw UnexpectedError on 403', () async {
+      client.simulateForbiddenError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw UnexpectedError on 404', () async {
+      client.simulateNotFoundError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw UnexpectedError on 500', () async {
+      client.simulateServerError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
     });
   });
 }
