809b72c16547dfc3328e946043238a5652426fbe
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-22 09:14:43 -0400

    change Mappers to use base class

--- lib/infra/api/mappers/next_event_mapper.dart
+++ lib/infra/api/mappers/next_event_mapper.dart
@@ -1,13 +1,15 @@
 import 'package:advanced_flutter/domain/entities/next_event.dart';
+import 'package:advanced_flutter/infra/api/mappers/mapper.dart';
 import 'package:advanced_flutter/infra/api/mappers/next_event_player_mapper.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
-final class NextEventMapper {
-  static NextEvent toObject(Json json) {
+final class NextEventMapper extends Mapper<NextEvent> {
+  @override
+  NextEvent toObject(Json json) {
     return NextEvent(
       groupName: json['groupName'],
       date: DateTime.parse(json['date']),
-      players: NextEventPalyerMapper.toList(json['players']),
+      players: NextEventPalyerMapper().toList(json['players']),
     );
   }
 }
--- lib/infra/api/mappers/next_event_player_mapper.dart
+++ lib/infra/api/mappers/next_event_player_mapper.dart
@@ -1,11 +1,10 @@
 import 'package:advanced_flutter/domain/entities/next_event_player.dart';
+import 'package:advanced_flutter/infra/api/mappers/mapper.dart';
 import 'package:advanced_flutter/infra/types/json.dart';
 
-final class NextEventPalyerMapper {
-  static List<NextEventPlayer> toList(JsonArr arr) =>
-      arr.map(NextEventPalyerMapper.toObject).toList();
-
-  static NextEventPlayer toObject(Json json) => NextEventPlayer(
+final class NextEventPalyerMapper extends Mapper<NextEventPlayer> {
+  @override
+  NextEventPlayer toObject(Json json) => NextEventPlayer(
     id: json['id'],
     name: json['name'],
     isConfirmed: json['isConfirmed'],
