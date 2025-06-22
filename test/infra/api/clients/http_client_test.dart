03725276e0f1e9adecbd22d9ab8a12fe8226070b
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-06-22 08:08:12 -0400

    ensure HttpClient returns a List

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:advanced_flutter/domain/entities/domain_error.dart';
+import 'package:advanced_flutter/infra/types/json.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -27,7 +28,12 @@ class HttpClient {
     final response = await client.get(uri, headers: allHeaders);
     switch (response.statusCode) {
       case 200:
-        return jsonDecode(response.body);
+        {
+          final data = jsonDecode(response.body);
+          return (T == JsonArr)
+              ? data.map<Json>((e) => e as Json).toList()
+              : data;
+        }
       case 401:
         throw DomainError.sessionExpired;
       default:
@@ -155,10 +161,24 @@ void main() {
       expect(future, throwsA(DomainError.unexpected));
     });
 
-    test('should return a map', () async {
+    test('should return a Map', () async {
       final data = await sut.get(url: url);
       expect(data['key1'], 'value1');
       expect(data['key2'], 'value2');
+    });
+
+    test('should return a List', () async {
+      client.responseJson = '''
+    [{
+      "key": "value1"
+    },
+    {
+      "key": "value2"
+    }]
+    ''';
+      final data = await sut.get(url: url);
+      expect(data[0]['key'], 'value1');
+      expect(data[1]['key'], 'value2');
     });
   });
 }
